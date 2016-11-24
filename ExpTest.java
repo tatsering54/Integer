795305520c999a3789265ba2c6ef8c31153a7342
tashi tsering <tatsering54@gmail.com>
2016-11-24 18:09:05 +0530

    Add files via upload

---
+++ ExpTest.java
@@ -0,0 +1,14 @@
+class ExpTest
+{
+	public static void main ( String[] args)
+	{
+		int a = 20; 
+		int b = 10; b = (a-- - --a);
+		int c = a--;
+		int d = (a >> 2);
+		int e = a&b;
+	
+	System.out.println("a :" + a + " b :"+ b + " c : "+ c + " d :" + c + " d : " + d +" e : "+ e);
+	}
+}
+		
